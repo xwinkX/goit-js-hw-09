ad5562c6a3485210865679814df7853c9ccf8d71
xwinkX <opalyuk.e88@ukr.net>
2022-06-03 22:20:53 +0300

    update_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
 
+startEl.addEventListener('click', startColor);
+stopEl.addEventListener('click', stopColor);
+
+function startColor() {
+  startEl.disabled = true;
+  switcherId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.classList.add('background-color');
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopColor() {
+  clearInterval(switcherId);
+  startEl.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+console.log(getRandomHexColor());
