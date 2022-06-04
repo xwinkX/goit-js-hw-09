ff63033c3dc3d94fe684c655eac3f548d322611f
xwinkX <opalyuk.e88@ukr.net>
2022-06-04 08:52:50 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,19 @@
 const startEl = document.querySelector('[data-start]');
 const stopEl = document.querySelector('[data-stop]');
 
-startEl.addEventListener('click', startColor);
-stopEl.addEventListener('click', stopColor);
-
-function startColor() {
+startEl.addEventListener('click', () => {
   startEl.disabled = true;
   switcherId = setInterval(() => {
     const color = getRandomHexColor();
     document.body.classList.add('background-color');
     document.body.style.backgroundColor = color;
   }, 1000);
-}
+});
 
-function stopColor() {
+stopEl.addEventListener('click', () => {
   clearInterval(switcherId);
   startEl.disabled = false;
-}
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
