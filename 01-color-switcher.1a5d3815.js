9aa10c3164d7dcef806cb4b434194fb3b2cdf919
xwinkX <95179630+xwinkX@users.noreply.github.com>
2022-06-04 05:53:29 +0000

    Deploying to gh-pages from @ xwinkX/goit-js-hw-09@ff63033c3dc3d94fe684c655eac3f548d322611f 🚀

---
+++ 01-color-switcher.1a5d3815.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(()=>{t.disabled=!0,switcherId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.classList.add("background-color"),document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(switcherId),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.1a5d3815.js.map
