96f88e0317e06b5c8e5f695a6e94a827e13fb953
xwinkX <opalyuk.e88@ukr.net>
2022-06-05 21:33:39 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+const submitForm = document.querySelector('.form');
+const delayEl = document.querySelector('[name=delay]');
+const steepEl = document.querySelector('[name=step]');
+const amountEl = document.querySelector('[name=amount]');
+let position = 0;
+
+submitForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  setTimeout(() => {
+    for (let i = 0; i < amountEl.value; i += 1) {
+      const value = +delayEl.value + +steepEl.value * i;
+      position = i + 1;
+      createPromise(position, value)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  }, delayEl.value);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
